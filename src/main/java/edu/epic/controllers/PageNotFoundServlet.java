1f49cae9f2206939e187f7b80f5d9e730a435a39
vidurapriyadarshana <vidura200427@gmail.com>
2026-03-24 09:58:08 +0530

    Add PageNotFoundServlet to handle 404 errors

---
+++ src/main/java/edu/epic/controllers/PageNotFoundServlet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package edu.epic.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet(name = "PageNotFoundServlet", urlPatterns = {"/PageNotFoundServlet"})
+public class PageNotFoundServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+}
